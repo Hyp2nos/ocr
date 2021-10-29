74acc75604967123211144366ec5029d3b1644d2
Andriamiharisoa Mamy Fifaliana <85287596+Hyp2nos@users.noreply.github.com>
2021-10-29 09:54:04 +0300

    OCR 80%

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {createWorker } from 'tesseract.js';
-import { element } from 'protractor';
+import {AlertController, LoadingController, ModalController, ToastController} from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,7 @@ import { element } from 'protractor';
 export class HomePage {
  worker: Tesseract.Worker;
  workerReady = false;
- image = '../../assets/modele-facture-fr-pur-750px.png';  //'https://tesseract.projectnaptha.com/img/eng_bw.png';
+ image = '../../assets/modele-facture-proforma.png';  //'https://tesseract.projectnaptha.com/img/eng_bw.png';
  ocrResult = '';
  totalHT:number =0;
  totalTVA=0;
@@ -19,7 +20,10 @@ export class HomePage {
  date = '';
  numeroDeFact = '';
 
-  constructor() {
+  constructor(
+    public loadingCtrl: LoadingController,
+    public toastController: ToastController
+  ) {
     this.loadWorker();
   }
 
@@ -44,6 +48,14 @@ export class HomePage {
   }
 
   async recognizeImage(){
+
+      const loading = await this.loadingCtrl.create({
+      message: 'loading..',
+      showBackdrop: true,
+      spinner: 'circles'
+    });
+
+    await loading.present();
      const result = await this.worker.recognize(this.image);
      console.log(result);
      let re = /,/gi;
@@ -51,11 +63,10 @@ export class HomePage {
      let re3 = /\n/gi;
      this.ocrResult = result.data.text.replace(re,".");
      this.ocrResult = result.data.text.replace(re2," ");
-    
      this.ocrResult = result.data.text.replace(re3," ");
     
      if((this.ocrResult.includes("HT") && this.ocrResult.includes("TVA"))|| (this.ocrResult.includes("TVA") && this.ocrResult.includes("TTC"))|| (this.ocrResult.includes("tva") && this.ocrResult.includes("ttc") ) || (this.ocrResult.includes("invoice") && this.ocrResult.includes("ttc")&& this.ocrResult.includes("tva")) ){
-          console.log("Oui c'est un facture")
+        
         
           const resultArray =this.ocrResult.split(" ");
        
@@ -65,7 +76,7 @@ export class HomePage {
           console.log(i +" " +element.toLowerCase());
          
           //Total HT1 ---------------------------------------
-          if(element.toLowerCase() ==="total" || element.toLowerCase() ==="totai" ){
+          if(element.toLowerCase() ==="total" || element.toLowerCase() ==="totai" || element.toLowerCase() === "montant" ){
             if (resultArray[i+ 1].toLowerCase() === "ht") {
                if (!isNaN(parseFloat(resultArray[i+1+1]))) {
                  this.totalHT = parseFloat(resultArray[i+1+1]);
@@ -98,10 +109,7 @@ export class HomePage {
             }
           }
            
-          //Total Ht 2 ------------------------------------
-          
-          
-          
+          //Total Ht 2 ------------------------------------ 
            if(element.toLowerCase() ==="totalht" || element.toLowerCase() === "totaiht" ){    
                   
                if (!isNaN(parseFloat(resultArray[i+1]))) {
@@ -116,31 +124,69 @@ export class HomePage {
                  }
                }
           } 
-
-
-          
-
-         /*  if (element.toLowerCase()==="total") {
+          //TOTAL TVA-----------------------------------
+           if (element.toLowerCase()==="total") {
              if (resultArray[i+1].toLowerCase() === "tva") {
-               if (!isNaN(parseFloat(resultArray[i+1+1]))) {
-                 this.totalHT = parseFloat(resultArray[i+1+1]);
-                 alert("totalTVA :"+this.totalTVA)
-               }
-               else{
-                 if (!isNaN(parseFloat(resultArray[i+1+1+1]))) {
-                    this.totalHT = parseFloat(resultArray[i+1+1+1]);
-                 alert("totalTVA :"+this.totalTVA)
+               if (resultArray[i+1+1].toLowerCase().indexOf('%') !== -1) {
+                  if (!isNaN(parseFloat(resultArray[i+1+1+1]))) {
+                  this.totalTVA = parseFloat(resultArray[i+1+1+1]);    
+                  }
+                  else{
+                  if(!isNaN(parseFloat(resultArray[i+1+1+1+1]))) {
+                    this.totalTVA = parseFloat(resultArray[i+1+1+1+1]);
+                  }
                  }
-               }
+                }
+               else{
+                  if (!isNaN(parseFloat(resultArray[i+1+1]))) {
+                  this.totalTVA = parseFloat(resultArray[i+1+1]);    
+                  }
+                  else{
+                  if(!isNaN(parseFloat(resultArray[i+1+1+1]))) {
+                    this.totalTVA = parseFloat(resultArray[i+1+1+1]);
+                  }
+                 }
+               } 
             }
           }
-
-          if (element.toLowerCase()==="tva200%") {
-             if (!isNaN(parseFloat(resultArray[i+1]))) {
+          //TVA 2
+           if (element.toLowerCase() === "tva") {
+               if (resultArray[i+1].toLowerCase().indexOf('%') !== -1) {
+                  if (!isNaN(parseFloat(resultArray[i+1+1]))) {
+                  this.totalTVA = parseFloat(resultArray[i+1+1]);    
+                  }
+                  else{
+                  if(!isNaN(parseFloat(resultArray[i+1+1+1]))) {
+                    this.totalTVA = parseFloat(resultArray[i+1+1+1]);
+                  }
+                 }
+                }
+               else{
+                  if (!isNaN(parseFloat(resultArray[i+1]))) {
+                  this.totalTVA = parseFloat(resultArray[i+1]);    
+                  }
+                  else{
+                  if(!isNaN(parseFloat(resultArray[i+1+1]))) {
+                    this.totalTVA = parseFloat(resultArray[i+1+1]);
+                  }
+                 }
+               } 
+            }
+            //Total TVA 2
+            if(element.toLowerCase() ==="totaltva" || element.toLowerCase() === "totaitva" ){    
+                  
+               if (!isNaN(parseFloat(resultArray[i+1]))) {
                  this.totalTVA = parseFloat(resultArray[i+1]);
-                 alert("total TVA :" + this.totalTVA);
-             }
-          } */
+               }else{
+                 if (!isNaN(parseFloat(resultArray[i+1+1]))) {
+                    this.totalTVA = parseFloat(resultArray[i+1+1]);
+                 }else{
+                   if (!isNaN(parseFloat(resultArray[i+1+1+1]))) {
+                     this.totalTVA = parseFloat(resultArray[i+1+1+1])
+                   }
+                 }
+               }
+          } 
 
          //Total ---------------------------------------------------
           if (element.toLowerCase() === "total"|| element.toLowerCase() === "totai" || element.toLowerCase() === "totalttc" ) {
@@ -171,7 +217,20 @@ export class HomePage {
             }
           }
 
-
+          if (element.toLowerCase() === "total") {
+            if (resultArray[i+1+1].toLowerCase() === "de" ) {
+              if (resultArray[i+1+1+1].toLowerCase() === "la" ) {
+                if (resultArray[i+1+1+1+1].toLowerCase() === "facture" ) {
+                  if (!isNaN(parseFloat(resultArray[i+1+1+1+1+1]))) {
+                    this.total = parseFloat(resultArray[i+1+1+1+1+1])
+                  }
+                }   
+              }
+            }
+            else{
+              
+            }
+          }
 
         //Date --------------------------------------------------------------------    
            if (element.toLowerCase() === "date" && this.date == '' && resultArray[i+1].toLowerCase() !== "de") {
@@ -196,8 +255,6 @@ export class HomePage {
             }
           }
           
-
-
           //Numero de facture--------------------------------------------------------
           if (element.toLowerCase() === "n°" || element.toLowerCase() === "numéro" || element.toLowerCase() === "référence" ) {
             if (resultArray[i+1].toLowerCase() === "de") {
@@ -224,15 +281,37 @@ export class HomePage {
           }
 
         }
-     }
-     else{
-       console.log("Non ce n'est pas un facture")
-     }
-     this.totalTVA = this.total - this.totalHT;
+
+        if (this.total === 0 && this.totalHT !== 0 && this.totalTVA !== 0) {
+          this.total = this.totalHT + this.totalTVA;
+        }
+        if (this.totalTVA === 0 && this.totalHT !== 0 && this.total !==0) {
+          this.totalTVA = this.total - this.totalHT;
+        }
+     
+      await loading.dismiss();
+        const toast = await this.toastController.create({
+          message: 'transaction ajoutée! ',
+          duration: 2000,
+          color: 'success',
+        });
+        await toast.present();
      alert("total TVA :" + this.totalTVA);
      alert("Total HT final :" + this.totalHT);
      alert("Total final :" + this.total);
      alert("Numero de facture :" + this.numeroDeFact);
      alert("date :" + this.date);
+     }
+
+     else{
+        console.log("ce n'est pas un facture");
+        await loading.dismiss();
+        const toast = await this.toastController.create({
+          message: ' ☣ une erreur est survenue, ressayer! ',
+          duration: 2000,
+          color: 'danger',
+        });
+        await toast.present();
+     }
   }
 }
